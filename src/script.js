9b1a971ac722db69315c63bf6b05f575ac7c645c
Prateek Rai <raip4687@gmail.com>
2026-08-05 18:07:45 +0530

    Designed TODO list

--- src/script.js
+++ src/script.js
@@ -14,3 +14,48 @@ function openFeatures() {
   });
 }
 openFeatures();
+
+let tasks = [];
+
+let frm = document.querySelector(".todo-frm");
+let input = document.querySelector(".inp");
+let alltsks = document.querySelector(".all-tasks");
+frm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (input.value == "") {
+    return;
+  }
+  tasks.push({ task: input.value });
+  renderTask();
+  frm.reset();
+});
+function renderTask() {
+  let task = "";
+  tasks.forEach(function (e) {
+    task += `<div class="bg-slate-800 rounded-xl p-4 flex justify-between items-center">
+
+                <div class="flex items-center gap-4">
+
+                    <input type="checkbox" class="check w-5 h-5 accent-cyan-500">
+
+                    <p class="text-white">
+                        ${e.task}
+                    </p>
+
+                </div>
+
+                <div class="flex gap-3">
+
+                    
+
+                    <button class="mark text-red-500 hover:scale-110 duration-300">
+                        🗑️
+                    </button>
+
+                </div>
+
+            </div>`;
+  });
+  alltsks.innerHTML = task;
+}
